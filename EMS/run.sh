76b61d595285b5c6760184c0622f68537200c679
Sourav Goswami <souravgoswami@protonmail.com>
2020-03-11 14:04:03 +0530

    Fixed run.sh to cd into the correct directory

--- EMS/run.sh
+++ EMS/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cd src && /bin/java -cp commons-codec-1.13.jar:sqlite-jdbc-3.27.2.1.jar:. Login >/dev/null 2>&1 &
+cd `dirname $0`/src && /bin/java -cp commons-codec-1.13.jar:sqlite-jdbc-3.27.2.1.jar:. Login >/dev/null 2>&1 &
